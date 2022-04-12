0caf52ad595050c463790edd9c84dfe321dbd0fe
frontend <gabriellennon@wizsolucoes.com.br>
2022-04-12 10:07:51 -0300

    validation if cpf already exist

--- src/index.js
+++ src/index.js
@@ -8,12 +8,19 @@ const customers = [];
 
 app.post("/account", (request, response) => {
     const { cpf,name } = request.body;
-    const id = uuidv4();
+
+    // the "some" make a search and return true or false
+    // comparing if already exists the cpf recieved
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if(customerAlreadyExists){
+        return response.status(400).json({ error: "Customer already exists!" });
+    }
 
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     });
 
